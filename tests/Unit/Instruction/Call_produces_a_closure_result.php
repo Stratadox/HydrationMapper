7b90b91ae3c2e116b80ef47f9b550ff9e29d94fe
Stratadox <jesse@stratadox.com>
2018-01-21 10:42:12 +0100

    Add test for call instruction

---
+++ tests/Unit/Instruction/Call_produces_a_closure_result.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stratadox\Hydration\Mapper\Test\Unit\Instruction;
+
+use PHPUnit\Framework\TestCase;
+use Stratadox\Hydration\Mapper\Instruction\Call;
+use Stratadox\Hydration\Mapping\Property\Dynamic\ClosureResult;
+use function strlen;
+use function var_export;
+
+/**
+ * @covers \Stratadox\Hydration\Mapper\Instruction\Call
+ */
+class Call_produces_a_closure_result extends TestCase
+{
+    /** @scenario */
+    function producing_a_closure_mapping()
+    {
+        self::assertEquals(
+            ClosureResult::inProperty('version', function ($data) {
+                return strlen($data['id']);
+            }),
+            Call::the(function ($data) {
+                return strlen($data['id']);
+            })->followFor('version')
+        );
+    }
+
+    /** @scenario */
+    function producing_another_closure_mapping()
+    {
+        self::assertEquals(
+            ClosureResult::inProperty('data', function ($data) {
+                return var_export($data, true);
+            }),
+            Call::the(function ($data) {
+                return var_export($data, true);
+            })->followFor('data')
+        );
+    }
+}
