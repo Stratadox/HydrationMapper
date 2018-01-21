519e7f6945622f6da488c1e6feda6f2a0533643a
Stratadox <jesse@stratadox.com>
2018-01-21 06:45:37 +0100

    Add test for in (key) instruction

---
+++ tests/Unit/Instruction/In_changes_the_key.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stratadox\Hydration\Mapper\Test\Unit\Instruction;
+
+use PHPUnit\Framework\TestCase;
+use Stratadox\Hydration\Mapper\Instruction\In;
+use Stratadox\Hydration\Mapping\Property\Scalar\StringValue;
+
+/**
+ * @covers \Stratadox\Hydration\Mapper\Instruction\In
+ */
+class In_changes_the_key extends TestCase
+{
+    /** @scenario */
+    function using_the_value_from_a_different_key()
+    {
+        self::assertEquals(
+            StringValue::inPropertyWithDifferentKey('foo', 'bar'),
+            In::key('bar')->followFor('foo')
+        );
+        self::assertEquals(
+            StringValue::inPropertyWithDifferentKey('bar', 'baz'),
+            In::key('baz')->followFor('bar')
+        );
+    }
+}
