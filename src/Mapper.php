3f437806fa6aa77c5a53e8e70c434a803debf514
Stratadox <jesse@stratadox.com>
2018-01-21 05:36:20 +0100

    Implement (very) basic mapper

---
+++ src/Mapper.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stratadox\Hydration\Mapper;
+
+use Stratadox\Hydration\Mapping\Mapping;
+use Stratadox\Hydration\Mapping\Property\Scalar\StringValue;
+use Stratadox\Hydration\MapsObject;
+
+final class Mapper
+{
+    private $name;
+    private $properties;
+
+    private function __construct(string $name, array $properties = [])
+    {
+        $this->name = $name;
+        $this->properties = $properties;
+    }
+
+    public static function forThe(string $className) : self
+    {
+        return new self($className);
+    }
+
+    public function property(
+        string $property,
+        InstructsHowToMap $instruction = null
+    ) : self
+    {
+        return new self($this->name, $this->add($property, $instruction));
+    }
+
+    public function map() : MapsObject
+    {
+        $class = $this->name;
+        $properties = [];
+        foreach ($this->properties as $name => $instruction) {
+            // @todo maybe follow instructions?
+            $properties[] = StringValue::inProperty($name);
+        }
+        return Mapping::ofThe($class, ...$properties);
+    }
+
+    private function add(string $property, InstructsHowToMap $instruction = null) : array
+    {
+        return $this->properties + [$property => $instruction];
+    }
+}
