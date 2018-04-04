2b5e8f8a0fc47f84ba50941ba8ac4999966d65fe
Stratadox <info@stratadox.com>
2018-04-04 02:32:42 +0200

    Add test for Mapper with non-existing class

--- tests/Unit/Mapper_collects_and_maps_properties.php
+++ tests/Unit/Mapper_collects_and_maps_properties.php
@@ -11,10 +11,13 @@ use Stratadox\Hydration\Mapper\Test\Stub\Instruction\ItsANumber;
 use Stratadox\Hydration\Mapping\Properties;
 use Stratadox\Hydration\Mapping\Property\Scalar\IntegerValue;
 use Stratadox\Hydration\Mapping\Property\Scalar\StringValue;
+use Stratadox\HydrationMapper\InvalidMapperConfiguration;
 use Stratadox\Hydrator\MappedHydrator;
 
 /**
  * @covers \Stratadox\Hydration\Mapper\Mapper
+ *
+ * @covers \Stratadox\Hydration\Mapper\NoSuchClass
  */
 class Mapper_collects_and_maps_properties extends TestCase
 {
@@ -46,5 +49,17 @@ class Mapper_collects_and_maps_properties extends TestCase
                 ->property('number', ItsANumber::allRight())
                 ->finish()
         );
+    }
+
+    /** @test */
+    function cannot_map_non_existing_classes()
+    {
+        $this->expectException(InvalidMapperConfiguration::class);
+        $this->expectExceptionCode(0);
+        $this->expectExceptionMessage(
+            'Could not produce mapping for non-existing class ' .
+            '`Stratadox\Not\An\Actual\Class`'
+        );
+        Mapper::forThe('Stratadox\Not\An\Actual\Class')->finish();
     }
 }
